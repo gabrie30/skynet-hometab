4a08e4db2c6e7d962bd55e01c9b135218ea646b0
Jay Gabriels <jgabriels@us.westfield.com>
2017-04-04 13:54:15 -0700

    Update navbar.jsx

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="nav-button-left">
-        <div className="nav-button-text-left"><a className="nav-button" target="_blank" href="https://westfieldlabs.okta.com/app/UserHome">okta</a></div>
+        <div className="nav-button-text-left"><a className="nav-button" target="_blank" href="https://jira.westfieldlabs.com/secure/Dashboard.jspa">jira</a></div>
       </div>
       <div className="nav-button-right">
         <div className="nav-button-text-right"><a className="nav-button" target="_blank" href="http://www.gmail.com">gmail</a></div>
